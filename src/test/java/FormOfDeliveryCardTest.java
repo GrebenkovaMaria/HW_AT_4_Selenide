6a21be70f6e13018ef1b82fa236f912341404a4b
Maria Grebenkova <grebenkova.m.p@gmail.com>
2021-04-24 23:54:21 +0300

    successful tests for attempt of CI on appveyor

--- src/test/java/FormOfDeliveryCardTest.java
+++ src/test/java/FormOfDeliveryCardTest.java
@@ -1,35 +1,281 @@
 import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.openqa.selenium.Keys;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.openqa.selenium.Keys.BACK_SPACE;
 
 public class FormOfDeliveryCardTest {
 
+    @BeforeEach
+    void openUrl() {
+        open("http://localhost:9999");
+    }
+
+    @AfterEach
+    void tearDown() {
+        closeWindow();
+    }
+
+
     @Test
     void shouldTestSuccessOrderIfCorrectFilling() {
-       LocalDate localDate = LocalDate.now().plusDays(5);
-       String  date = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(localDate);
-        open("http://localhost:9999");
+        LocalDate localDate = LocalDate.now().plusDays(5);
+        String date = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(localDate);
         $("[data-test-id='city'] .input__control").setValue("Ульяновск");
-        $("[data-test-id='date'] .input__control").click();
-        $("[data-test-id='date'] .input__control").click();
-        $("[data-test-id='date'] .input__control").clear();
-       // $("[data-test-id='date'] .input__control").sendKeys(date);
+        $("[data-test-id='date'] .input__control").doubleClick();
+        $("[data-test-id='date'] .input__control")
+                .sendKeys(Keys.chord(BACK_SPACE,
+                        date));
         $("[data-test-id='name'] .input__control").setValue("Мария Гребенькова");
         $("[data-test-id='phone'] .input__control").setValue("+79876543210");
         $$(".checkbox__box").find(Condition.visible).click();
         $$("button").find(Condition.exactText("Забронировать")).click();
         $(withText("Встреча успешно забронирована"))
                 .shouldBe(Condition.visible, Duration.ofSeconds(15));
-
+        $(withText("Встреча успешно забронирована"))
+                .shouldBe(Condition.text(date));
     }
 
+    @Test
+    void shouldTestSuccessOrderIfPlusThreeDays() {
+        LocalDate localDate = LocalDate.now().plusDays(3);
+        String date = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(localDate);
+        $("[data-test-id='city'] .input__control").setValue("Ульяновск");
+        $("[data-test-id='date'] .input__control").doubleClick();
+        $("[data-test-id='date'] .input__control")
+                .sendKeys(Keys.chord(BACK_SPACE,
+                        date));
+        $("[data-test-id='name'] .input__control").setValue("Мария Гребенькова");
+        $("[data-test-id='phone'] .input__control").setValue("+79876543210");
+        $$(".checkbox__box").find(Condition.visible).click();
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        $(withText("Встреча успешно забронирована"))
+                .shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(withText("Встреча успешно забронирована"))
+                .shouldBe(Condition.text(date));
+    }
 
+    @Test
+    void shouldTestSuccessOrderIfPlus365Days() {
+        LocalDate localDate = LocalDate.now().plusDays(365);
+        String date = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(localDate);
+        $("[data-test-id='city'] .input__control").setValue("Ульяновск");
+        $("[data-test-id='date'] .input__control").doubleClick();
+        $("[data-test-id='date'] .input__control")
+                .sendKeys(Keys.chord(BACK_SPACE,
+                        date));
+        $("[data-test-id='name'] .input__control").setValue("Мария Гребенькова");
+        $("[data-test-id='phone'] .input__control").setValue("+79876543210");
+        $$(".checkbox__box").find(Condition.visible).click();
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        $(withText("Встреча успешно забронирована"))
+                .shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(withText("Встреча успешно забронирована"))
+                .shouldBe(Condition.text(date));
+    }
+
+    @Test
+    void shouldTestUnsuccessOrderIfPlusTwoDays() {
+        LocalDate localDate = LocalDate.now().plusDays(2);
+        String date = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(localDate);
+        $("[data-test-id='city'] .input__control").setValue("Ульяновск");
+        $("[data-test-id='date'] .input__control").doubleClick();
+        $("[data-test-id='date'] .input__control")
+                .sendKeys(Keys.chord(BACK_SPACE,
+                        date));
+        $("[data-test-id='name'] .input__control").setValue("Мария Гребенькова");
+        $("[data-test-id='phone'] .input__control").setValue("+79876543210");
+        $$(".checkbox__box").find(Condition.visible).click();
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        $(withText("Заказ на выбранную дату невозможен"))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldTestUnsuccessOrderIfNoName() {
+        LocalDate localDate = LocalDate.now().plusDays(3);
+        String date = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(localDate);
+        $("[data-test-id='city'] .input__control").setValue("Ульяновск");
+        $("[data-test-id='date'] .input__control").doubleClick();
+        $("[data-test-id='date'] .input__control")
+                .sendKeys(Keys.chord(BACK_SPACE,
+                        date));
+        $("[data-test-id='phone'] .input__control").setValue("+79876543210");
+        $$(".checkbox__box").find(Condition.visible).click();
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        $(withText("Поле обязательно для заполнения"))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldTestUnsuccessOrderIfNoCity() {
+        LocalDate localDate = LocalDate.now().plusDays(3);
+        String date = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(localDate);
+        $("[data-test-id='date'] .input__control").doubleClick();
+        $("[data-test-id='date'] .input__control")
+                .sendKeys(Keys.chord(BACK_SPACE,
+                        date));
+        $("[data-test-id='name'] .input__control").setValue("Мария Гребенькова");
+        $("[data-test-id='phone'] .input__control").setValue("+79876543210");
+        $$(".checkbox__box").find(Condition.visible).click();
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        $(withText("Поле обязательно для заполнения"))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldTestUnsuccessOrderIfNoNumber() {
+        LocalDate localDate = LocalDate.now().plusDays(365);
+        String date = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(localDate);
+        $("[data-test-id='city'] .input__control").setValue("Ульяновск");
+        $("[data-test-id='date'] .input__control").doubleClick();
+        $("[data-test-id='date'] .input__control")
+                .sendKeys(Keys.chord(BACK_SPACE,
+                        date));
+        $("[data-test-id='name'] .input__control").setValue("Мария Гребенькова");
+        $$(".checkbox__box").find(Condition.visible).click();
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        $(withText("Поле обязательно для заполнения"))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldTestUnsuccessOrderIfNoDate() {
+        $("[data-test-id='city'] .input__control").setValue("Ульяновск");
+        $("[data-test-id='date'] .input__control").doubleClick();
+        $("[data-test-id='date'] .input__control")
+                .sendKeys(Keys.chord(BACK_SPACE));
+        $("[data-test-id='name'] .input__control").setValue("Мария Гребенькова");
+        $$(".checkbox__box").find(Condition.visible).click();
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        $(withText("Неверно введена дата"))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldTestUnsuccessOrderIfCityNotFromList() {
+        LocalDate localDate = LocalDate.now().plusDays(3);
+        String date = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(localDate);
+        $("[data-test-id='city'] .input__control").setValue("Минск");
+        $("[data-test-id='date'] .input__control").doubleClick();
+        $("[data-test-id='date'] .input__control")
+                .sendKeys(Keys.chord(BACK_SPACE,
+                        date));
+        $("[data-test-id='name'] .input__control").setValue("Мария Гребенькова");
+        $("[data-test-id='phone'] .input__control").setValue("+79876543210");
+        $$(".checkbox__box").find(Condition.visible).click();
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        $(withText("Доставка в выбранный город недоступна"))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldTestUnsuccessOrderIfNameInEnglish() {
+        LocalDate localDate = LocalDate.now().plusDays(3);
+        String date = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(localDate);
+        $("[data-test-id='city'] .input__control").setValue("Ульяновск");
+        $("[data-test-id='date'] .input__control").doubleClick();
+        $("[data-test-id='date'] .input__control")
+                .sendKeys(Keys.chord(BACK_SPACE,
+                        date));
+        $("[data-test-id='name'] .input__control").setValue("Maria Grebenkova");
+        $("[data-test-id='phone'] .input__control").setValue("+79876543210");
+        $$(".checkbox__box").find(Condition.visible).click();
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        $(withText("только русские буквы, пробелы и дефисы"))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldTestUnsuccessOrderIfTelWithoutPlusOnStart() {
+        LocalDate localDate = LocalDate.now().plusDays(3);
+        String date = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(localDate);
+        $("[data-test-id='city'] .input__control").setValue("Ульяновск");
+        $("[data-test-id='date'] .input__control").doubleClick();
+        $("[data-test-id='date'] .input__control")
+                .sendKeys(Keys.chord(BACK_SPACE,
+                        date));
+        $("[data-test-id='name'] .input__control").setValue("Мария Гребенькова");
+        $("[data-test-id='phone'] .input__control").setValue("79876543210");
+        $$(".checkbox__box").find(Condition.visible).click();
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        $(withText("Телефон указан неверно"))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldTestUnsuccessOrderIfTelNotContainsElevenFigures() {
+        LocalDate localDate = LocalDate.now().plusDays(3);
+        String date = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(localDate);
+        $("[data-test-id='city'] .input__control").setValue("Ульяновск");
+        $("[data-test-id='date'] .input__control").doubleClick();
+        $("[data-test-id='date'] .input__control")
+                .sendKeys(Keys.chord(BACK_SPACE,
+                        date));
+        $("[data-test-id='name'] .input__control").setValue("Мария Гребенькова");
+        $("[data-test-id='phone'] .input__control").setValue("+7987654321");
+        $$(".checkbox__box").find(Condition.visible).click();
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        $(withText("Телефон указан неверно"))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldTestUnsuccessOrderIfNoClickOnCofirmation() {
+        LocalDate localDate = LocalDate.now().plusDays(3);
+        String date = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(localDate);
+        $("[data-test-id='city'] .input__control").setValue("Ульяновск");
+        $("[data-test-id='date'] .input__control").doubleClick();
+        $("[data-test-id='date'] .input__control")
+                .sendKeys(Keys.chord(BACK_SPACE,
+                        date));
+        $("[data-test-id='name'] .input__control").setValue("Мария Гребенькова");
+        $("[data-test-id='phone'] .input__control").setValue("+79876543210");
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        String text = $(".checkbox__text").getCssValue("color");
+        assertEquals("rgba(255, 92, 92, 1)", text);
+    }
+
+//    @Test
+//    void shouldTestSuccessOrderIfNameWithSpaceAndHyphen() {
+//        LocalDate localDate = LocalDate.now().plusDays(3);
+//        String date = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(localDate);
+//        $("[data-test-id='city'] .input__control").setValue("Ульяновск");
+//        $("[data-test-id='date'] .input__control").doubleClick();
+//        $("[data-test-id='date'] .input__control")
+//                .sendKeys(Keys.chord(BACK_SPACE,
+//                        date));
+//        $("[data-test-id='name'] .input__control").setValue("Mария Гребенькова-Ушакова");
+//        $("[data-test-id='phone'] .input__control").setValue("+79876543210");
+//        $$(".checkbox__box").find(Condition.visible).click();
+//        $$("button").find(Condition.exactText("Забронировать")).click();
+//        $(withText("Встреча успешно забронирована"))
+//                .shouldBe(Condition.visible, Duration.ofSeconds(15));
+//    }
+//
+//    @Test
+//    void shouldTestSuccessOrderIfNameWithLetterYo() {
+//        LocalDate localDate = LocalDate.now().plusDays(3);
+//        String date = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(localDate);
+//        $("[data-test-id='city'] .input__control").setValue("Ульяновск");
+//        $("[data-test-id='date'] .input__control").doubleClick();
+//        $("[data-test-id='date'] .input__control")
+//                .sendKeys(Keys.chord(BACK_SPACE,
+//                        date));
+//        $("[data-test-id='name'] .input__control").setValue("Mария Гребёнькова");
+//        $("[data-test-id='phone'] .input__control").setValue("+79876543210");
+//        $$(".checkbox__box").find(Condition.visible).click();
+//        $$("button").find(Condition.exactText("Забронировать")).click();
+//        $(withText("Встреча успешно забронирована"))
+//                .shouldBe(Condition.visible, Duration.ofSeconds(15));
+//    }
 }
 
 
